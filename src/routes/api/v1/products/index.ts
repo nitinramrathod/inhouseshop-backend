adf056b31a3b4ee724649b5a68c619a9f5840c70
Nitin Rathod <rathodni3ram4967@gmail.com>
2025-12-23 23:17:30 +0530

    post and list product api tested

---
+++ src/routes/api/v1/products/index.ts
@@ -0,0 +1,36 @@
+import { FastifyInstance } from "fastify";
+import {
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+} from "../../../../controllers/product.controller";
+
+
+export default async function productRoutes(fastify: FastifyInstance) {
+  fastify.post(
+    "/",
+    createProduct
+  );
+
+  fastify.get("/", getProducts);
+
+  fastify.get(
+    "/:id",
+    
+    getProductById
+  );
+
+  fastify.put(
+    "/:id",
+   
+    updateProduct
+  );
+
+  fastify.delete(
+    "/:id",
+    
+    deleteProduct
+  );
+}
