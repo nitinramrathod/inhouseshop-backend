5c41a747eeb6bbcf3d94cdaf452d98940091f8b7
Nitin Rathod <rathodni3ram4967@gmail.com>
2025-12-28 14:15:51 +0530

    mongodb uri added in env plugin

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -8,12 +8,16 @@ export default fp(async (app) => {
       type: "object",
       required: [
         "MONGODB_URI",
+        "JWT_SECRET",
+        "PORT",
         "CLOUDINARY_CLOUD_NAME",
         "CLOUDINARY_API_KEY",
         "CLOUDINARY_API_SECRET"
       ],
       properties: {
         MONGODB_URI: { type: "string" },
+        JWT_SECRET: { type: "string" },
+        PORT: { type: "string" },
         CLOUDINARY_CLOUD_NAME: { type: "string" },
         CLOUDINARY_API_KEY: { type: "string" },
         CLOUDINARY_API_SECRET: { type: "string" }
