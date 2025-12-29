2e4d3454c9c38fdbddcd632b9dc399025a3ae00d
Nitin Rathod <rathodni3ram4967@gmail.com>
2025-12-29 23:23:07 +0530

    some changes added

--- src/plugins/env.ts
+++ src/plugins/env.ts
@@ -1,27 +1,27 @@
-import fp from "fastify-plugin";
-import fastifyEnv from "@fastify/env";
+// import fp from "fastify-plugin";
+// import fastifyEnv from "@fastify/env";
 
-export default fp(async (app) => {
-  await app.register(fastifyEnv, {
-    dotenv: true,
-    schema: {
-      type: "object",
-      required: [
-        "MONGODB_URI",
-        "JWT_SECRET",
-        "PORT",
-        "CLOUDINARY_CLOUD_NAME",
-        "CLOUDINARY_API_KEY",
-        "CLOUDINARY_API_SECRET"
-      ],
-      properties: {
-        MONGODB_URI: { type: "string" },
-        JWT_SECRET: { type: "string" },
-        PORT: { type: "string" },
-        CLOUDINARY_CLOUD_NAME: { type: "string" },
-        CLOUDINARY_API_KEY: { type: "string" },
-        CLOUDINARY_API_SECRET: { type: "string" }
-      }
-    }
-  });
-});
+// export default fp(async (app) => {
+//   await app.register(fastifyEnv, {
+//     dotenv: true,
+//     schema: {
+//       type: "object",
+//       required: [
+//         "MONGODB_URI",
+//         "JWT_SECRET",
+//         "PORT",
+//         "CLOUDINARY_CLOUD_NAME",
+//         "CLOUDINARY_API_KEY",
+//         "CLOUDINARY_API_SECRET"
+//       ],
+//       properties: {
+//         MONGODB_URI: { type: "string" },
+//         JWT_SECRET: { type: "string" },
+//         PORT: { type: "string" },
+//         CLOUDINARY_CLOUD_NAME: { type: "string" },
+//         CLOUDINARY_API_KEY: { type: "string" },
+//         CLOUDINARY_API_SECRET: { type: "string" }
+//       }
+//     }
+//   });
+// });
