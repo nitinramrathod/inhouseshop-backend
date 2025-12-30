b85728bbbb197be8002633b14e125ce8b1c9ec4d
Nitin Rathod <rathodni3ram4967@gmail.com>
2025-12-30 20:09:59 +0530

    commented code removed

--- src/config/dbconnect.ts
+++ src/config/dbconnect.ts
@@ -9,23 +9,3 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
-
-// import mongoose from 'mongoose'
-// import { FastifyInstance } from 'fastify'
-
-// try {
-//   process.loadEnvFile()
-// } catch {}
-
-// export async function connectDB(app: FastifyInstance) {
-//   console.log('app.config==>',app.config)
-//   console.log('process.env==>',process.env)
-//   const uri = process.env.MONGODB_URI
-
-//   if (!uri) {
-//     throw new Error('MONGODB_URI is missing')
-//   }
-
-//   await mongoose.connect(uri)
-//   app.log.info('MongoDB connected successfully')
-// }
