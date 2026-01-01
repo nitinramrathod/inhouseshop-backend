729af09da296e8a4b1036be50e4098b4b9c8f3e7
Nitin Rathod <rathodni3ram4967@gmail.com>
2026-01-01 18:31:35 +0530

    create user api chanages added

--- src/routes/api/v1/users/index.ts
+++ src/routes/api/v1/users/index.ts
@@ -3,8 +3,8 @@ import UserController from "../../../../controllers/user.controller";
 
 export default async function userRoutes(fastify: FastifyInstance) {
   fastify.post(
-    "/",
-    { preHandler: fastify.authenticate },
+    "/register",
+    // { preHandler: fastify.authenticate },
     UserController.createUser
   );
 
